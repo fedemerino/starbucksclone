e4acb4d4995615a6a90e68c1e2d5bf1e500236e3
Federico Merino <fedemerino00@gmail.com>
2023-02-11 00:31:08 -0300

    11/2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <div className="navbarLeft">
-                <img src="/starbucks.svg" alt="starbuckslogo" />
-                <ul className='menu'>
-                    <li>MENÚ</li>
-                    <li>CAFÉ</li>
-                    <li>EXPERIENCIA STARBUCKS</li>
-                </ul>
-            </div>
-            <div className="navbarRight">
-                <a href="https://www.starbucks.com.ar/stores" target={'_blank'}><div className='localizarTienda'>
-                    <img src="/pin.svg" alt="pin" style={{ 'height': '1.275rem' }} /> <p style={{'padding-left':'5px'}}>Localizar Tienda</p>
-                </div></a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="navbarLeft">
+        <Link to={"/"}>
+          <img src="/starbucks.svg" alt="starbuckslogo" />
+        </Link>
+        <ul className="menu">
+          <li>
+            <Link to={"/menu"} className={"navMenu"}>
+              MENÚ
+            </Link>
+          </li>
+          <Link to={"/#"} className={"navMenu"}>
+            CAFÉ
+          </Link>
+          <li>
+            <Link to={"/#"} className={"navExp"}>
+              EXPERIENCIA STARBUCKS
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbarRight">
+        <a href="https://www.starbucks.com.ar/stores" target={"_blank"}>
+          <div className="localizarTienda">
+            <img src="/pin.svg" alt="pin" style={{ height: "1.275rem" }} />{" "}
+            <p style={{ "padding-left": "5px" }}>Localizar Tienda</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
