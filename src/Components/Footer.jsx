ca2ca9e8ce34569b6567574188c8e6bfce251e54
Federico Merino <fedemerino00@gmail.com>
2023-02-23 02:24:27 -0300

    Footer Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Footer = () => {
+  const [sobreNosotrosButton, setSobreNosotrosButton] = useState(true);
+  const [impactoSocialButton, setImpactoSocialButton] = useState(true);
+  const [atencionButton, setAtencionButton] = useState(true);
+  const [experienciaButton, setExperienciaButton] = useState(true);
+
+  const handleButton = (state, setState) => {
+    setState(!state);
+  };
+
   return (
     <div className="footer">
       <div className="footerTop">
@@ -8,35 +17,85 @@ const Footer = () => {
           <div className="aboutUs">
             <div className="footerTitle">
               <p>Sobre nosotros</p>
-            <img src="/arrow-down.svg" alt="" /></div>
-            <p>Nuestra historia</p>
-            <p>Nuestra misión</p>
-            <p>Nuestra cultura inclusiva y valores</p>
-            <p>Nuestro café</p>
-            <p>Historias y novedades</p>
-            <p>Comunicados oficiales</p>
+              <img
+                src="/arrow-down.svg"
+                alt=""
+                onClick={() =>
+                  handleButton(sobreNosotrosButton, setSobreNosotrosButton)
+                }
+              />
+            </div>
+            {sobreNosotrosButton
+              ? null
+              : [
+                  <>
+                    <p>Nuestra historia</p>
+                    <p>Nuestra misión</p>
+                    <p>Nuestra cultura inclusiva y valores</p>
+                    <p>Nuestro café</p>
+                    <p>Historias y novedades</p>
+                    <p>Comunicados oficiales</p>
+                  </>,
+                ]}
           </div>
           <div className="socialImpact">
             <div className="footerTitle">
               <p>Impacto social</p>
-            <img src="/arrow-down.svg" alt="" /></div>
-            <p>Planeta</p>
-            <p>Personas</p>
+              <img
+                src="/arrow-down.svg"
+                alt=""
+                onClick={() =>
+                  handleButton(impactoSocialButton, setImpactoSocialButton)
+                }
+              />
+            </div>
+            {impactoSocialButton
+              ? null
+              : [
+                  <>
+                    <p>Planeta</p>
+                    <p>Personas</p>
+                  </>,
+                ]}
           </div>
           <div className="support">
             <div className="footerTitle">
               <p>Atención al cliente</p>
-            <img src="/arrow-down.svg" alt="" /></div>
-            <p>Contacto</p>
-            <p>Medios de pago</p>
-            <p>Defensa de las y los consumidores</p>
+              <img
+                src="/arrow-down.svg"
+                alt=""
+                onClick={() => handleButton(atencionButton, setAtencionButton)}
+              />
+            </div>
+            {atencionButton
+              ? null
+              : [
+                  <>
+                    <p>Contacto</p>
+                    <p>Medios de pago</p>
+                    <p>Defensa de las y los consumidores</p>
+                  </>,
+                ]}
           </div>
           <div className="experience">
             <div className="footerTitle">
               <p>Experiencia Starbucks</p>
-            <img src="/arrow-down.svg" alt="" /></div>
-            <p>Formas de comprar</p>
-            <p>Delivery</p>
+              <img
+                src="/arrow-down.svg"
+                alt=""
+                onClick={() =>
+                  handleButton(experienciaButton, setExperienciaButton)
+                }
+              />
+            </div>
+            {experienciaButton
+              ? null
+              : [
+                  <>
+                    <p>Formas de comprar</p>
+                    <p>Delivery</p>
+                  </>
+                ]}
           </div>
           <div>&nbsp;</div>
           <div className="dataFiscal">
