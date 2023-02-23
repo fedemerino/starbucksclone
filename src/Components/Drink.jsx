4a0dd83176ea550136a102c28ffb31133125ca23
Federico Merino <fedemerino00@gmail.com>
2023-02-22 21:58:17 -0300

    mediaqueriesdone

--- src/Components/Drink.jsx
+++ src/Components/Drink.jsx
@@ -4,7 +4,7 @@ const Drink = ({ name, img, link, id}) => {
     let address = `/${link}/${id}`
     return (
 
-        <div className='col col-lg-4 drinkContainer'>
+        <div className='col col-lg-4 col-md-6 col-sm-6 col-6 drinkContainer'>
             <Link to={address}>
                 <div className='drinkCircle'>
                     <img src={img} alt={name} />
